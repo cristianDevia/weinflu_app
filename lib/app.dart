fbab8ecd8aa45327040958515e9125d39fc79063
Cristian Devia <c.devia8@gmail.com>
2024-04-02 15:45:23 -0500

    create app class

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:weinflu_app/desing/theme.dart';
+import 'package:weinflu_app/pages/home_page.dart';
+
+class WeinFluApp extends StatelessWidget {
+  const WeinFluApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WeinFlu App',
+      theme: WeinFluThemes.defaultTheme,
+      home: const HomePage(),
+    );
+  }
+}
