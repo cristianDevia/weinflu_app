2c9586365a783924c5842b04c14109bd5542eeba
Cristian Devia <c.devia8@gmail.com>
2024-04-02 17:27:28 -0500

    add radius styles

---
+++ lib/desing/radius.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class WeinfluRadius {
+  WeinfluRadius._();
+  static const Radius xs = Radius.circular(12);
+  static const Radius small = Radius.circular(12);
+}
