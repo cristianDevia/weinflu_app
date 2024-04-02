f6d57a1883b22a9ad19da1d0a80f623523a56668
Cristian Devia <c.devia8@gmail.com>
2024-04-02 17:26:41 -0500

    add two new colors

--- lib/desing/colors.dart
+++ lib/desing/colors.dart
@@ -5,6 +5,9 @@ class WeinfluColors {
   static const Color brandPrimaryColor = Color.fromRGBO(53, 97, 254, 1);
   static const Color brandSecondaryColor = Color.fromRGBO(243, 246, 255, 1);
   static const Color brandLightColor = Colors.white;
+  static const Color brandLightColorBorder = Color.fromRGBO(210, 228, 255, 1);
+  static const Color brandLightBackgraundColor =
+      Color.fromRGBO(244, 246, 254, 1);
   static const Color brandLightColorOpacity =
       Color.fromRGBO(255, 255, 255, 0.2);
   static const Color brandDarkColor = Colors.black;
